9791b09ff38ce0db3a2035f2bfe73e0d7a6ff1fe
ZilverCoder <zilvergamer@gmail.com>
2018-09-24 19:12:07 +0200

    Started with TS

--- public/compiled/js/main.js
+++ public/compiled/js/main.js
@@ -1,94 +1,53 @@
-/*
-var taskValue = ko.observable();
-var taskIdentifier = ko.observable();
-var taskCompleted = ko.observable();
-var taskList = ko.observableArray();
-*/
-
-var TodoApp = function(){
-	var self = this;
-	var task = function(taskValue, taskCompleted){
-		this.value = ko.observable(taskValue);
-		this.completed = ko.observable(taskCompleted);
-	}
-	//#region Custom Ko Bindings
-	var ENTER_KEY = 13;
-	var ESCAPE_KEY = 27;
-	function keyhandlerBindingFactory(keyCode) {
-		return {
-			init: function (element, valueAccessor, allBindingsAccessor, data, bindingContext) {
-				var wrappedHandler, newValueAccessor;
-
-				// wrap the handler with a check for the enter key
-				wrappedHandler = function (data, event) {
-					if (event.keyCode === keyCode) {
-						valueAccessor().call(this, data, event);
-					}
-				};
-
-				// create a valueAccessor with the options that we would want to pass to the event binding
-				newValueAccessor = function () {
-					return {
-						keyup: wrappedHandler
-					};
-				};
-
-				// call the real event binding's init function
-				ko.bindingHandlers.event.init(element, newValueAccessor, allBindingsAccessor, data, bindingContext);
-			}
-		};
-	}
-	ko.bindingHandlers.enterKey = keyhandlerBindingFactory(ENTER_KEY);
-	//#endregion
-	
-	//#region Functions
-		self.taskValue = ko.observable();
-		self.taskList = ko.observableArray();
-		self.addTask = function(){
-			self.taskList.push(new task(self.taskValue(), false));
-			self.taskValue('');
-		}.bind(this);
-		self.removeTask = function(task) {
-			self.taskList.remove(task);
-		}.bind(this);
-		this.clearCompletedTasks = function() {
-			this.taskList.remove((task) => {
-				return task.completed();
-		  });
-		}.bind(this);
-		self.remainingTasks = ko.computed(function(){
-			return self.taskList().filter(function (task) {
-				return !task.completed();
-			}).length + " items left";
-		}, this);
-	//#endregion
-	
-	//#region Filters
-		self.filterMode = ko.observable('all')
-		self.filteredTaskList = ko.computed(function(){
-			switch(this.filterMode()){
-				case 'todo':
-					return self.taskList().filter(function(task){
-						return !task.completed();
-					});
-				case 'done':
-					return self.taskList().filter(function(task){
-						return task.completed();
-					});
-				default:
-					return self.taskList();
-			}
-		}, this);
-		self.changeToAll = function(){
-			self.filterMode('all');
-		}
-		self.changeToActive = function(){
-			self.filterMode('todo');
-		}
-		self.changeToCompleted = function() {
-			self.filterMode('done');
-		}
-	//#endregion	
-}
-
-ko.applyBindings(new TodoApp());
+define(["require", "exports", "knockout"], function (require, exports, ko) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Task = /** @class */ (function () {
+        function Task(value, completed) {
+            this.value = ko.observable(value);
+            this.completed = ko.observable(completed);
+        }
+        Task.prototype.task = function () {
+            return this.value, this.completed;
+        };
+        return Task;
+    }());
+    var TodoApp = /** @class */ (function () {
+        function TodoApp() {
+            var self = this;
+            var ENTER_KEY = 13;
+            function keyhandlerBindingFactory(keyCode) {
+                return {
+                    init: function (element, valueAccessor, allBindingsAccessor, data, bindingContext) {
+                        var wrappedHandler;
+                        var newValueAccessor;
+                        // wrap the handler with a check for the enter key
+                        wrappedHandler = function (data, event) {
+                            if (event.keyCode === keyCode) {
+                                valueAccessor().call(this, data, event);
+                            }
+                        };
+                        // create a valueAccessor with the options that we would want to pass to the event binding
+                        newValueAccessor = function () {
+                            return {
+                                keyup: wrappedHandler
+                            };
+                        };
+                        // call the real event binding's init function
+                        ko.bindingHandlers.event.init(element, newValueAccessor, allBindingsAccessor, data, bindingContext);
+                    }
+                };
+            }
+            ko.bindingHandlers.enterKey = keyhandlerBindingFactory(ENTER_KEY);
+            //#region Functions
+            self.taskValue = ko.observable();
+            self.taskList = ko.observableArray();
+            self.addTask = (function () {
+                self.taskList.push(new Task(self.taskValue(), false));
+                self.taskValue('');
+            }).bind(this);
+            //#endregion
+        }
+        return TodoApp;
+    }());
+    ko.applyBindings(new TodoApp());
+});
--- public/js/main.ts
+++ public/js/main.ts
@@ -1,36 +1,59 @@
 import * as ko from "knockout";
 
-/* class Task{
-	taskValue: KnockoutObservable<string>
-	taskIdentifier: KnockoutObservable<string>
-	taskCompleted: KnockoutObservable<boolean>
-	taskList: KnockoutObservableArray<object>
-	constructor(taskValue: string, taskIdentifier: string, taskCompleted: boolean) {
-		this.taskValue = ko.observable();
-		this.taskIdentifier = ko.observable();
-		this.taskCompleted = ko.observable(false);
+class Task {
+	value: KnockoutObservable<string>
+	completed: KnockoutObservable<boolean>
+	constructor(value: string, completed: boolean){
+		this.value = ko.observable(value);
+		this.completed = ko.observable(completed);
 	}
-	makeNew
-} */
+	task() {
+		return this.value, this.completed;
+	}
+}
 
 class TodoApp {
 	taskValue: KnockoutObservable<string>
-	taskIdentifier: KnockoutObservable<string>
 	taskCompleted: KnockoutObservable<boolean>
-	taskList: KnockoutObservableArray<string[]>
+	taskList: KnockoutObservableArray<object>
 	//Functions
 	addTask: KnockoutComputed<string>
 
     constructor() {
-		var task = function(taskValue, completed){
-			this.taskValue = ko.observable(taskValue);
-			this.taskCompleted = ko.observable(completed);
-		};
-		this.taskList = ko.observableArray();
-		this.taskValue = ko.observable();
-		this.addTask = ko.computed(function(){
-			return this.taskList().push(this.taskValue());
-		}, this);
+		let self = this;
+		let ENTER_KEY = 13;
+		function keyhandlerBindingFactory(keyCode: number) {
+			return {
+				init: function (element: any, valueAccessor: any, allBindingsAccessor: any, data: any, bindingContext: any) {
+					let wrappedHandler: any;
+					let newValueAccessor: any;
+					// wrap the handler with a check for the enter key
+					wrappedHandler = function (data: any, event: any) {
+						if (event.keyCode === keyCode) {
+							valueAccessor().call(this, data, event);
+						}
+					};
+					// create a valueAccessor with the options that we would want to pass to the event binding
+					newValueAccessor = function () {
+						return {
+							keyup: wrappedHandler
+						};
+					};
+					// call the real event binding's init function
+					ko.bindingHandlers.event.init(element, newValueAccessor, allBindingsAccessor, data, bindingContext);
+				}
+			};
+		}
+		ko.bindingHandlers.enterKey = keyhandlerBindingFactory(ENTER_KEY);
+		//#region Functions
+			self.taskValue = ko.observable();
+			self.taskList = ko.observableArray();
+			self.addTask = (() => {
+				self.taskList.push(new Task(self.taskValue(), false));
+				self.taskValue('');
+			}).bind(this);
+
+		//#endregion
     }
 }
 
